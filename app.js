12102146ab71043302b8d94c5d93c01aba04c6d3
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-12-08 14:14:07 +0500

    Implemented level 1 Authentication

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+// ------------------------------------------DATABASE Stuff-------------------------------------------
+
+// connect database
+mongoose.connect("mongodb://localhost:27017/userDB")
+    .then(() => {console.log("Database Connected....")})
+    .catch((e) => {console.log(e)});
+
+
+// Create a Schema
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+// Register a model/Collection
+const User = mongoose.model("User", userSchema);
+
+// -------------------------------------------------------------------------------------------------------
 
 
 
@@ -23,6 +45,32 @@ app.get("/register", (req, res) => {
 });
 
 
+app.post("/register", function(req, res){
+    newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    })
+
+    newUser.save().then(
+        ()=> {
+            res.render("secrets");
+        } 
+    ).catch( (err)=>{console.log(err)} );
+
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then( (foundUser) => {
+        if (foundUser.password === password){
+            console.log("User authenticated successfully....");
+            res.render("secrets");
+        }
+    }).catch( (err) => { console.log(err)} );
+
+});
 
 
 
---
+++ problem.md
@@ -0,0 +1,5 @@
+# Authenction Level 1
+
+### Problem
+
+The problem is that we can see the user **passwords** in **plain text** because if I have **millions of users** and the password is in **plain text**, anyone of my **employees** can look at my **database** and know what the password is. Or, if a **hacker** decides to hack into my **server**, locate my **database**, then this is a pretty good **loot** for them.

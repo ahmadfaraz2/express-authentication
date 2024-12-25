eb1da36cfcff6540e25c15baeecd29d1c56d0387
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-12-25 23:12:11 +0500

    Implemented Level 4 authentication (Hashing + Salting)

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -48,35 +50,46 @@ app.get("/register", (req, res) => {
 
 app.post("/register", function(req, res){
     const email = req.body.username;
-    const password = md5(req.body.password);
+    const plainPassword = req.body.password;
 
-    const newUser = User({
-        email: email,
-        password: password
+    bcrypt.hash(plainPassword, saltRounds)
+    .then((hash) => {
+        const newUser = User({
+            email: email,
+            password: hash,
+        });
+
+        return newUser.save();
+    })
+    .then( () => {
+        console.log("User registered successfully...");
+        res.render("secrets");
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newUser.save().then(
-        ()=> {
-            console.log("User registered Successfully...");
-            res.render("secrets");
-        } 
-    ).catch( (err)=>{console.log(err)} );
 
 });
 
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const plainPassword = req.body.password;
 
-    User.findOne({email: username}).then( (foundUser) => {
-        if (foundUser.password === password){
-            console.log("User authenticated Successfully....");
-            res.render("secrets");
-        }
-    } ).catch( (err) => {
-        console.log(err);
-    } )
+    User.findOne({email: username}).then((foundUser) => {
+        
+        bcrypt.compare(plainPassword, foundUser.password)
+        .then((result) => {
+            if (result === true) {
+                console.log("User authenticated Successfully....");
+                res.render("secrets");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    });
 
 });
 
